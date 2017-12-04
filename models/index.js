4f89d8c72580f8975293c2486cf2fb3d93ec59f7
Mohamed Habshey <Mohamed.habshey10@gmail.com>
2017-12-04 17:44:09 +0200

    add modal index

---
+++ models/index.js
@@ -0,0 +1,38 @@
+"use strict";
+
+var fs = require("fs");
+var path = require("path");
+var Sequelize = require("sequelize");
+var basename = path.basename(module.filename);
+var sequelize = new Sequelize(process.env.JAWSDB_URL, {
+  dialect: 'mysql',
+  define: {
+    //prevent sequelize from pluralizing table names
+    freezeTableName: true
+  },
+  dialectOptions: {
+    ssl: true
+  }
+});
+var db = {};
+
+fs
+  .readdirSync(__dirname)
+  .filter(function (file) {
+    return (file.indexOf(".") !== 0) && (file !== basename);
+  })
+  .forEach(function (file) {
+    var model = sequelize["import"](path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(function (modelName) {
+  if ("associate" in db[modelName]) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
