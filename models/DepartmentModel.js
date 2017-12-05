6d1cc3340a23be015cdaf7fd473895259cb0f027
Mohamed Habshey <Mohamed.habshey10@gmail.com>
2017-12-05 18:08:18 +0200

    add department id as forigen key to advisor & students

--- models/DepartmentModel.js
+++ models/DepartmentModel.js
@@ -1,13 +1,24 @@
 'use strict';
 
 module.exports = function (sequelize, DataTypes) {
-    var Department = sequelize.define('department', {
+    var Department = sequelize.define('Department', {
         Dep_id: {
             type: DataTypes.INTEGER,
-            primaryKey: true
+            primaryKey: true,
+            autoIncrement: true
         }, //  department primary key 
         Dep_name: { type: DataTypes.STRING, allowNull: false } // department name
     });
+    Department.associate = function (models) {
+        models.Department.hasOne(models.ACA_ADV, {
+            foreignKeyConstraint: true,
+            foreignKey: 'Dep_id'
+        });
 
+        models.Department.hasOne(models.Students, {
+            foreignKeyConstraint: true,
+            foreignKey: 'Dep_id'
+        });
+    }
     return Department;
 }
