39f0b1da4368f0f19ddb93f576cef6b9c539587e
Mohamed Habshey <Mohamed.habshey10@gmail.com>
2017-12-05 18:07:37 +0200

    add advisor forigen key to department

--- models/ACA_ADVModel.js
+++ models/ACA_ADVModel.js
@@ -4,16 +4,21 @@ module.exports = function (sequelize, DataTypes) {
     var ACA_ADV = sequelize.define('ACA_ADV', {
         Aca_id: {
             type: DataTypes.INTEGER,
-            primaryKey: true
+            primaryKey: true,
+            autoIncrement: true
         }, //  ACA_ADV primary key 
         Aca_name: { type: DataTypes.STRING, allowNull: false }, // academic name
-        Dep_id: { type: DataTypes.INTEGER },
         password: { type: DataTypes.STRING, allowNull: false } // academic advisory's password
     });
     ACA_ADV.associate = function (models) {
         models.ACA_ADV.hasMany(models.Students, {
             foreignKeyConstraint: true,
             foreignKey: 'Aca_id'
+        });
+
+        models.ACA_ADV.belongsTo(models.Department, {
+            foreignKeyConstraint: true,
+            foreignKey: 'Dep_id'
         });
     }
     return ACA_ADV;
