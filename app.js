7ed5050849167cff28ac26c17853a4046c5d8c26
Mohamed Habshey <Mohamed.habshey10@gmail.com>
2017-12-06 18:14:27 +0200

    subject route

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var student = require('./routes/student');
 var advisor = require('./routes/advisor');
+var subject = require('./routes/subject');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/student', student);
 app.use('/advisor', advisor);
+app.use('/subject', subject);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ routes/subject.js
@@ -0,0 +1,101 @@
+var express = require('express');
+var router = express.Router();
+
+var models = require('../models');
+/* POST Student Create. */
+router.post('/subAdd', function (req, res, next) {
+    var SubData = req.body.SubData;
+
+    var subId = SubData.Sub_id;
+    var subName = SubData.Sub_name;
+    var subLevel = SubData.Level;
+    var subHour = SubData.Hour;
+    var subDepartment = SubData.Dep_id;
+
+    var subPreReqId = SubData.Pre_req_id;
+
+
+    models.Plan.findOrCreate({
+        where: { Sub_id: subId },
+        defaults: {
+            Pre_req_id: subPreReqId,
+            Sub_name: subName,
+            Dep_id: subDepartment,
+            Level: subLevel,
+            Hour: subHour
+        }
+    })
+        .spread((subject, created) => {
+            // console.log(student.get({
+            //   plain: true
+            // }))
+            // console.log(created)
+            if (created) {
+                res.send({ subject: subject });
+                // student.getDepartment()
+                // .then(function (department) {
+                //   student.dataValues.Dep_name = department.Dep_name;
+                //   res.send({ subject: subject });
+                // })
+
+            } else {
+                res.send({ error: "Subject with the same ID already exists!" });
+            }
+        }).catch(function () {
+            res.send({ error: "Unexpected error please try again!" });
+        })
+
+});
+router.post('/mySubjects', function (req, res, next) {
+    var StuId = req.body.StuId;
+
+
+    if (!StuId) return res.send({ error: "error missing params" });
+
+    models.SubStu.findAll({
+        where: { Stu_id: StuId },
+        order: [
+            // Will escape username and validate DESC against a list of valid direction parameters
+            [models.Plan, 'Level', 'DESC'],],
+        include: [
+            // include the subjects
+            { model: models.Plan, include: [models.Department,  { model: models.Plan, attributes: ["Sub_name", "Sub_id"], as: 'Subject' },] }
+        ]
+    })
+        .then(subjects => {
+            // 
+            if (!subjects.length) {
+                res.send({ error: "No Subjects assigned to this student yet!!" });
+            } else {
+                res.send({ subjects: subjects });
+            }
+        }).catch(function (error) {
+            res.send({ error: "Unexpected error please try again!" });
+        })
+
+});
+
+router.post('/allSubjects', function (req, res, next) {
+
+
+    models.Plan.findAll({
+        include: [
+            // include the subjects
+            { model: models.Plan, attributes: ["Sub_name", "Sub_id"], as: 'Subject' },
+            { model: models.Department }
+        ]
+    })
+        .then(subjects => {
+            // 
+            if (!subjects.length) {
+                res.send({ error: "No Subjects added yet!!" });
+            } else {
+                res.send({ subjects: subjects });
+            }
+        }).catch(function (error) {
+            res.send({ error: "Unexpected error please try again!" });
+        })
+
+});
+
+module.exports = router;
