81a56de9745b372b953ea5136ab339acec8babf1
Mohamed Habshey <Mohamed.habshey10@gmail.com>
2017-12-07 18:41:26 +0200

    request stubject model enhancments

--- models/Req_Stu.js
+++ models/Req_Stu.js
@@ -1,27 +1,25 @@
 module.exports = function (sequelize, DataTypes) {
 
     var Req_Stu = sequelize.define('Req_Stu', {
-        Stu_id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        Stu_name: { type: DataTypes.STRING, allowNull: false },
-        Sub_name: { type: DataTypes.STRING, allowNull: false },
-        Level: { type: DataTypes.INTEGER, allowNull: false },
-        ACA_ID: { type: DataTypes.INTEGER }
+       
+        Reason: { type: DataTypes.STRING, allowNull: false }
+      
     });
 
     Req_Stu.associate = function (models) {
-        models.Req_Stu.belongsTo(models.SubStu, {
+        models.Req_Stu.belongsTo(models.Students, {
             foreignKeyConstraint: true,
             foreignKey: 'Stu_id'
         });
 
+        models.Req_Stu.belongsTo(models.Plan, {
+            foreignKeyConstraint: true,
+            foreignKey: 'Sub_id'
+        });
 
         models.Req_Stu.belongsTo(models.ACA_ADV, {
             foreignKeyConstraint: true,
-            foreignKey: 'ACA_ID'
+            foreignKey: 'Aca_ID'
         });
     }
     return Req_Stu;
